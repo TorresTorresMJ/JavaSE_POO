db11bf6739a37c9cb070ef2b3d5ce3a75ae176e4
Mónica Jocelyn Torres <monicatorrest@MacBook-Air-de-Monica.local>
2025-07-25 14:47:46 -0600

    Agrega MyMedicalApp con su gitignore especifico

---
+++ MyMedicalApp/src/Doctor.java
@@ -0,0 +1,18 @@
+public class Doctor {
+    int id;
+    String name;
+    String speciality;
+
+    Doctor (){
+        System.out.println("Construyendo el objeto Doctor");
+    }
+
+    Doctor (String name){
+        System.out.println("Nombre del doctor ");
+    }
+
+    //Comportamientos
+    public void showName(){
+        System.out.println(name);
+    }
+}
