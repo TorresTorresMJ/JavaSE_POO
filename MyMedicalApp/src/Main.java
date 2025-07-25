68f3dd089223ae563ed7f110f72e5013a64ec9e8
TorresTorresMJ <monicajtorrestorres@gmail.com>
2025-07-25 16:38:19 -0600

    Variable final para constante

--- MyMedicalApp/src/Main.java
+++ MyMedicalApp/src/Main.java
@@ -2,19 +2,6 @@ import static UI.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        Doctor myDoctor = new Doctor();
-        myDoctor.name = "Alejandro Rodriguez";
-        myDoctor.showName();
-        myDoctor.showId();
-        System.out.println(Doctor.id);
-
-        //Doctor.id++; Afectando el identificador desde otra clase
-
-        Doctor MyDoctorAnn = new Doctor();
-        myDoctor.showId();
-        System.out.println(Doctor.id);
 
         showMenu();
     }
